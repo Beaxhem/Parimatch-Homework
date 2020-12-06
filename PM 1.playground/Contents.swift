152959ab61c729bafd20882f36ebdf609cd0f0a7
Ilya Senchukov <senchukov02@gmail.com>
2020-12-06 16:18:47 +0200

    First homework done (i think so)

---
+++ PM 1.playground/Contents.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+func factorialRecursive(n: Int) -> Int {
+    return n == 1 ? 1 : n * factorialRecursive(n: n-1)
+}
+
+factorialRecursive(n: 4)
+
+
+func factorialIterative(n: Int) -> Int {
+    var factorial = 1
+    var i = n
+
+    while i != 0 {
+        factorial *= i
+        i -= 1
+    }
+    
+    return factorial
+}
+
+factorialIterative(n: 4)
+
+
+func fib(n: Int) -> Int {
+    if n == 1 || n == 2 {
+        return 1
+    }
+    
+    return fib(n: n-1) + fib(n: n-2)
+}
+
+fib(n: 3)
+
+
+func piFraction(n: Int) -> Int {    
+    let piString = String(Double.pi)
+    
+    let startIndex = piString.index(piString.startIndex, offsetBy: n)
+    let endIndex = piString.index(piString.startIndex, offsetBy: n)
+    
+    return Int(piString[startIndex...endIndex])!
+    
+}
+
+piFraction(n: 16)
